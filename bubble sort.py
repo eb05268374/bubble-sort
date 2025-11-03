aa338a6e85673e3b59f02275652d04c06103c965
eb05 <eb05268374@priestley.ac.uk>
2025-11-03 10:16:43 +0000

    Refactor bubble sort functions and improve readability
    Refactor bubble sort implementation for clarity and conciseness.

--- bubble sort.py
+++ bubble sort.py
@@ -1,16 +1,14 @@
 # bubble sort
 
 def single_pass(index, lst):
-    for i in range(index+1, len(lst)):
+    for i in range(index+1, len(lst)): # iterate for every item AFTER the current one
         if lst[index] > lst[i]: # if its bigger than the one next to it
-            temp = lst[i]
-            lst[i] = lst[index]
-            lst[index] = temp # swap
+            lst[index], lst[i] = lst[i], lst[index]# swap
     return lst
     
 def bubble_sort(to_sort):
-    for i in range(0, len(to_sort)):
-        to_sort = single_pass(i, to_sort)
+    for i in range(0, len(to_sort)): # iterate for every item
+        to_sort = single_pass(i, to_sort) # do a pass
     return to_sort
 
 # debugging stuff
@@ -18,4 +16,3 @@ import random
 lst = [random.randint(0,1000) for _ in range(1000)]
 
 print(bubble_sort(lst))
-
